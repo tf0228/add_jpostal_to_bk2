949bc302f8b6b346878f66b565578bef1c489515
vagrant <vagrant@localhost.localdomain>
2020-08-22 03:45:36 +0000

    [Finish] jpostal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_073610) do
+ActiveRecord::Schema.define(version: 2020_08_22_033122) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 2020_08_17_073610) do
     t.datetime "updated_at", null: false
     t.text "introduction"
     t.string "profile_image_id"
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
